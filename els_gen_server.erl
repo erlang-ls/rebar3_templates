d5ac91077fcb9ab292984c0ffb0494c081ca359b
Roberto Aloi <robert.aloi@klarna.com>
2020-04-23 15:57:32 +0200

    Add template for gen_server

---
+++ els_gen_server.erl
@@ -0,0 +1,57 @@
+%%%=============================================================================
+%%% @doc The @@name@@ gen_server.
+%%% @end
+%%%=============================================================================
+
+-module(els_@@name@@).
+
+%%==============================================================================
+%% API
+%%==============================================================================
+-export([ start_link/1
+        ]).
+
+%%==============================================================================
+%% Callbacks for the gen_server behaviour
+%%==============================================================================
+-behaviour(gen_server).
+-export([ init/1
+        , handle_call/3
+        , handle_cast/2
+        , handle_info/2
+        ]).
+-type state() :: #{}.
+
+%%==============================================================================
+%% Macro Definitions
+%%==============================================================================
+-define(SERVER, ?MODULE).
+
+%%==============================================================================
+%% API
+%%==============================================================================
+-spec start_link() -> {ok, pid()}.
+start_link() ->
+  gen_server:start_link({local, ?SERVER}, ?MODULE, unused, []).
+
+%%==============================================================================
+%% Callbacks for the gen_server behaviour
+%%==============================================================================
+-spec init() ->
+        {ok, state()}.
+init() ->
+  {ok, #{}}.
+
+-spec handle_call(any(), {pid(), any()}, state()) ->
+        {reply, any(), state()} | {noreply, state()}.
+handle_call(_Request, _From, State) ->
+  {noreply, State}.
+
+-spec handle_cast(any(), state()) ->
+        {reply, any(), state()} | {noreply, state()}.
+handle_cast(_Request, State) ->
+  {noreply, State}.
+
+-spec handle_info(any(), state()) -> {noreply, state()}.
+handle_info(Request, State) ->
+  {noreply, State}.
